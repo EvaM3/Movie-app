ce1081bd1bbbb7202c2c9a8afa4ec014b7636447
EvaM3 <tora.buke@gmail.com>
2023-10-20 22:33:23 +0200

    Initial Commit

---
+++ Movie app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie app
+//
+//  Created by Eva Sira Madarasz on 20/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
